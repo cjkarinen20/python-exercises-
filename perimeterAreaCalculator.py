9d969ce8de18ea2801904c55a865072c029f16b9
cjkarinen20 <cjkarinen20@gmail.com>
2026-02-07 17:31:58 -0500

    "Automate the Boring Stuff" Ch 1. Projects
    Complete solutions to the "Rectangle Printer"
    and "Perimeter and Area Calculator" practice
    projects in Ch. 1 of the "Automate the Boring
    Stuff" Workbook.

---
+++ perimeterAreaCalculator.py
@@ -0,0 +1,10 @@
+print('Enter the width for the rectange:')
+
+width = int(input('>'))
+length = int(input('>'))
+
+area = str(width * length)
+perimeter = str(2 * (width + length))
+
+print('Area of the rectangle: ' + area)
+print('Perimeter of the rectangle: ' + perimeter)
---
+++ rectPrint.py
@@ -0,0 +1,6 @@
+print('Enter the width for the rectangle:')
+
+width = int(input('>'))
+
+for i in range(5):
+    print('0' * width)
