42515efe00cd0c5d92047b598b8ba583e6136d0d
cjkarinen20 <cjkarinen20@gmail.com>
2026-02-11 19:48:46 -0500

    "Automate the Boring Stuff" Ch. 3 "xmasTreePrint"
    Complete solution to "Automate the Boring Stuff"
    workbook chapter 3 "Christmas Tree Printer".

--- AutomateTheBoringStuff-Workbook/03-loops/xmasTreePrint.py
+++ AutomateTheBoringStuff-Workbook/03-loops/xmasTreePrint.py
@@ -4,10 +4,16 @@ print('Enter the tree size: ')
 
 treeSize = int(input())
 
-for row_num in range(1, treeSize + 1):
-    if random.randint(1, 4) == 1: 
-        print( ' ' * (treeSize - row_num) + 'o' * (row_num * 2 - 1))
-    else:
-        print( ' ' * (treeSize - row_num) + '^' * (row_num * 2 - 1))
+for row_num in range(treeSize):
     
+    spaces = " " * (treeSize - row_num - 1)
+    row = ""
+    for _ in range(2 * row_num + 1):
+            if random.randint(1, 4) == 1:
+                row += "o"
+            else:
+                row += "^"
+
+    print(spaces + row)
+            
 print((' ' * (treeSize - 1) + '#\n') * 2)
