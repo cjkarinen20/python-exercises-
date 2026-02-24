c5b279f7a066530bbb7ae46c3c18212725353d10
cjkarinen20 <cjkarinen20@gmail.com>
2026-02-24 15:56:25 -0500

    "ATBS" Ch.6 Example Programs and Practice
    Example programs from chapter 6 of
    "Automate The Boring Stuff", including the
    practice program: "Comma Code".

---
+++ AutomateTheBoringStuff/06-lists/practice-programs/commaCode.py
@@ -0,0 +1,24 @@
+import copy 
+
+def listString(list):
+    
+    commaList = []
+    
+    if list[0] == None:
+        return ' '
+    
+    for index, item in enumerate(list):
+        commaList.append(item)
+        if index < len(list) - 2:  
+            commaList.append(', ')
+        elif index == len(list) - 2:
+            commaList.append(', and ')
+        elif index == len(list) - 1:
+            break  
+        
+    commaStr = ''
+    commaStr = commaStr.join(commaList)
+    return commaStr
+
+letters = ['a', 'b', 'c', 'd', 'e']
+print(listString(letters))
